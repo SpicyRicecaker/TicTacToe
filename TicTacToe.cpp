62e3ebcaec9c848954eb0e3481bc5f28559f384c
Your Name <you@example.com>
2019-09-24 22:50:11 -0700

    Figuring out what to do with null user input...

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstring>
-#define X 2
-#define O 3
+#define X_PIECE 2
+#define O_PIECE 3
 
 void init(int (&board)[3][3], int& turn);
 void printBoard(int (&board)[3][3]);
@@ -36,6 +36,8 @@ int main(){
   
   int xWins = 0;
   int oWins = 0;
+
+  char decMove[4] = "";
     
   init(board, turn);
 
@@ -44,7 +46,20 @@ int main(){
   while(running){
     printBoard(board);
 
-    cout << "It is player " << turn + 1 << "'s turn!" << endl;
+    cout << "It's player " << turn + 1 << "'s turn!" << endl;
+
+    cin.get(decMove, 4);
+    cin.get();
+
+    while(strlen(decMove) != 2 || ((decMove[0]!='A') && (decMove[0]!='a') && (decMove[0]!='B') && (decMove[0]!='b') && (decMove[0]!='C') && (decMove[0]!='c')) || ((decMove[1]!='1') && (decMove[1]!='2') && (decMove[1]!='3'))){
+      cout << "Please enter a valid row letter followed by column number with no spaces in between. (e.g. A1)" << endl;
+      cin.get(decMove, 999);
+      cin.get();
+    }
+
+    cout << decMove << endl;
+
+    
     running = false;
   }
 
